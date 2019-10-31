2695305cdf001d477ea67d4e2dd9645ef1ef4b6a
terfno <sueda.takahito@gmail.com>
2019-10-31 09:42:20 +0900

    :+1: /webhook post

--- server.js
+++ server.js
@@ -3,3 +3,17 @@ const axios = require('axios')
 const dotenv = require('dotenv').config()
 const express = require('express')
 
+const port = process.env.port || 3000
+
+const config ={
+  channelSecret: process.env.channelSecret,
+  channelAccessToken: process.env.channelAccessToken
+}
+
+const app = express()
+
+app.post('/webhook', line.middleware(config), (req, res)=>{
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result)=>res.json(result))
+})
